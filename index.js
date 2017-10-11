a92abfb17f45417678c0eaaae4b3c0c13cb57dde
Unknown <oqoporu@gmail.com>
2017-10-11 09:25:57 +0300

    gus

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// dependecies
 const Koa = require('koa');
 const logger = require('koa-logger');
+const serve = require('koa-static');
+
+// server
 const app = new Koa();
 
 // logger
 app.use(logger());
+
+app.use(serve('uploads'));
 
 // response
 app.use(function *() {
